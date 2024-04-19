ae206f8b6e6678c827076af894a21cb75cb6352b
nandanchakraborty <nandancsebubt@gmail.com>
2024-04-19 17:10:34 +0600

    supplier table added

--- create_database.py
+++ create_database.py
@@ -4,6 +4,8 @@ def create_db():
     cur = con.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS employee(eid INTEGER PRIMARY KEY AUTOINCREMENT,name text, email text , gender  text, contact  text, dob text , doj  text, pass text , utype text , address text , salary text) " )
     con.commit()
+    cur.execute("CREATE TABLE IF NOT EXISTS supplier(invoice INTEGER PRIMARY KEY AUTOINCREMENT,name text, contact text , desc text ) ")
+    con.commit()
 
 
 create_db()    
